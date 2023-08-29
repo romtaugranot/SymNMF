f46e50a3f3d1d7c393bc256fed6e9918fcad7a3b
romtaugranot <100371590+romtaugranot@users.noreply.github.com>
2023-08-29 10:53:55 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import Extension, setup
 
-module = Extension("symnmfmodule", sources=['symnmfmoudle.c'])
+module = Extension("symnmfmodule", sources=['symnmfmodule.c'])
 setup(name='symnmfmodule',
      version='1.0',
      description='Python wrapper for custom C extension',
