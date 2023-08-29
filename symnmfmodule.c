031467574cfa5504a6201790dce4575589c02141
romtaugranot <100371590+romtaugranot@users.noreply.github.com>
2023-08-29 10:55:46 +0300

    Update symnmfmodule.c

--- symnmfmodule.c
+++ symnmfmodule.c
@@ -1,17 +1,25 @@
-static PyObject* sym_module_imp(PyObject *self, PyObject *args)
-{
+#define PY_SSIZE_T_CLEAN
+#include <Python.h>
 
-    if(!PyArg_ParseTuple(args, "", ...)) {
+#include "symnmf.h"
+
+static PyObject* sym_module_imp(PyObject *self, PyObject *args) {  
+    struct vector* data_points;
+    char* file_path;
+    if(!PyArg_ParseTuple(args, "s", &file_path)) {
         return NULL;
     }
+    printf("path is: %s\n", file_path);
+    data_points = read_data_points(file_path);
+    
 
-    return Py_BuildValue("O", NULL);
+    return Py_BuildValue("O", data_points);
 }
 
 static PyObject* ddg_module_imp(PyObject *self, PyObject *args)
 {
 
-    if(!PyArg_ParseTuple(args, "", ...)) {
+    if(!PyArg_ParseTuple(args, "", NULL)) {
         return NULL;
     }
 
@@ -21,7 +29,7 @@ static PyObject* ddg_module_imp(PyObject *self, PyObject *args)
 static PyObject* norm_module_imp(PyObject *self, PyObject *args)
 {
 
-    if(!PyArg_ParseTuple(args, "", ...)) {
+    if(!PyArg_ParseTuple(args, "", NULL)) {
         return NULL;
     }
 
@@ -31,33 +39,52 @@ static PyObject* norm_module_imp(PyObject *self, PyObject *args)
 static PyObject* symnmf_module_imp(PyObject *self, PyObject *args)
 {
 
-    if(!PyArg_ParseTuple(args, "", ...)) {
+    if(!PyArg_ParseTuple(args, "", NULL)) {
         return NULL;
     }
 
     return Py_BuildValue("O", NULL);
 }
 
+static PyObject* get_list(PyObject* self, PyObject* args)
+{
+    int N,r;
+    PyObject* python_val;
+    PyObject* python_int;
+    if (!PyArg_ParseTuple(args, "i", &N)) {
+        return NULL;
+    }
+    python_val = PyList_New(N);
+    for (int i = 0; i < N; ++i)
+    {
+        r = i;
+        python_int = Py_BuildValue("i", r);
+        PyList_SetItem(python_val, i, python_int);
+    }
+    return python_val;
+}
+
 static PyMethodDef symnmfMethods[] = {
-    {"sym",(PyCFunction) sym_module_imp, METH_VARARGS, PyDoc_STR("Calculate and output the similarity matrix.")},
-    {"ddg",(PyCFunction) ddg_module_imp, METH_VARARGS, PyDoc_STR("Calculate and output the Diagonal Degree Matrix.")},
-    {"norm",(PyCFunction) norm_module_imp, METH_VARARGS, PyDoc_STR("Calculate and output the normalized similarity matrix.")},
-    {"symnmf",(PyCFunction) symnmf_module_imp, METH_VARARGS, PyDoc_STR("An implementation of symnmf.")},
+    {"sym", sym_module_imp, METH_VARARGS, PyDoc_STR("Calculate and output the similarity matrix.")},
+    {"ddg", ddg_module_imp, METH_VARARGS, PyDoc_STR("Calculate and output the Diagonal Degree Matrix.")},
+    {"norm", norm_module_imp, METH_VARARGS, PyDoc_STR("Calculate and output the normalized similarity matrix.")},
+    {"symnmf", symnmf_module_imp, METH_VARARGS, PyDoc_STR("An implementation of symnmf.")},
+    {"get_list", get_list, METH_VARARGS, PyDoc_STR("get list of ints")},
     {NULL, NULL, 0, NULL}
 };
 
-static struct PyModuleDef symnmfmodule = {
+static struct PyModuleDef module = {
     PyModuleDef_HEAD_INIT,
-    "symnmf", /* name of module */
-    NULL, /* module documentation, may be NULL */
+    "symnmfmodule", /* name of module exposed to Python */
+    "SymNMF Python wrapper for C extension library", /* module documentation */
     -1,  /* size of per-interpreter state of the module, or -1 if the module keeps state in global variables. */
     symnmfMethods /* the PyMethodDef array from before containing the methods of the extension */
 };
 
-PyMODINIT_FUNC PyInit_symnmf(void)
+PyMODINIT_FUNC PyInit_symnmfmodule(void)
 {
     PyObject *m;
-    m = PyModule_Create(&symnmfmodule);
+    m = PyModule_Create(&module);
     if (!m) {
         return NULL;
     }
