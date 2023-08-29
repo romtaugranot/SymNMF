1ccffb67af52fc4901a7d724de9cccea406cb47f
romtaugranot <100371590+romtaugranot@users.noreply.github.com>
2023-08-29 10:55:01 +0300

    Update symnmf.h

--- symnmf.h
+++ symnmf.h
@@ -1,16 +1,27 @@
-/* Functions implemented in symnmfmodule.c */
-static PyObject* sym_module_imp(PyObject *self, PyObject *args);
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <ctype.h>
 
-static PyObject* ddg_module_imp(PyObject *self, PyObject *args);
+static int k = 0;
+static int N = 0;
+static int d = 1;
+static struct vector *backup_vectors;
 
-static PyObject* norm_module_imp(PyObject *self, PyObject *args);
+const double beta = 0.5;
+const int max_iter = 1000;
+const double eps = 0.0001;
 
-static PyObject* symnmf_module_imp(PyObject *self, PyObject *args);
+/* Structs definitions */
+struct entry {
+    double value;
+    struct entry *next;
+};
 
-PyMODINIT_FUNC PyInit_symnmf(void);
-
-/* Functions implemented in symnmf.c */
-int main(int argc, char *argv[]);
+struct vector {
+    struct vector *next;
+    struct entry *entries;
+};
 
 /* Goals */
 double** compute_similarity_matrix(struct vector* vectors);
@@ -20,7 +31,7 @@ double** initialize_H(double** W);
 double** optimize_H(double** W);
 
 /* Argument reading and processing functions */
-struct vector* read_data_points();
+struct vector* read_data_points(char* file_name);
 void mem_error();
 void free_entries(struct entry *head);
 void free_matrix(double** matrix, int rows);
@@ -31,3 +42,15 @@ void print_diagonal_matrix(double* diagonal, int size);
 double squared_dist(struct vector u, struct vector v);
 double** matrix_multiply(double** A, double** B, int m, int n, int p);
 double** transpose(double** matrix, int rows, int cols);
+
+
+/* Functions implemented in symnmfmodule.c */
+static PyObject* sym_module_imp(PyObject *self, PyObject *args);
+
+static PyObject* ddg_module_imp(PyObject *self, PyObject *args);
+
+static PyObject* norm_module_imp(PyObject *self, PyObject *args);
+
+static PyObject* symnmf_module_imp(PyObject *self, PyObject *args);
+
+PyMODINIT_FUNC PyInit_symnmf(void);
