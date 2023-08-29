4c952d2c2c19763e3ea258d32f56aa3656d871c3
romtaugranot <100371590+romtaugranot@users.noreply.github.com>
2023-08-29 10:55:35 +0300

    Update symnmf.py

--- symnmf.py
+++ symnmf.py
@@ -1,5 +1,6 @@
 import sys
 import numpy as np
+import csv
 import symnmfmodule
 
 
@@ -8,17 +9,16 @@ def algorithm():
     np.random.seed(0)
     k = int(sys.argv[1])
     goal = sys.argv[2]
-    file_name = sys.argv[3]
-    X = ...
+    file_path = sys.argv[3]
 
     if goal == 'sym':
-        A = symnmfmodule.sym(X)
+        A = symnmfmodule.sym(file_path)
     elif goal == 'ddg':
-        D = symnmfmodule.ddg(X)
+        D = symnmfmodule.ddg(file_path)
     elif goal == 'norm':
-        W = symnmfmodule.norm(X)
+        W = symnmfmodule.norm(file_path)
     else: # goal == 'symmnf'
-        H = symnmfmodule.symnmf(X)
+        H = symnmfmodule.symnmf(file_path)
 
 
 algorithm()
