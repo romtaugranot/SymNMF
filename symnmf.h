9b168c16763dab2bbc1002a4017642b9a62f4c2d
romtaugranot <100371590+romtaugranot@users.noreply.github.com>
2023-09-04 15:45:25 +0300

    Update symnmf.h

--- symnmf.h
+++ symnmf.h
@@ -1,17 +1,3 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <math.h>
-#include <ctype.h>
-
-static int k = 0;
-static int N = 0;
-static int d = 1;
-static struct vector *backup_vectors;
-
-const double beta = 0.5;
-const int max_iter = 1000;
-const double eps = 0.0001;
-
 /* Structs definitions */
 struct entry {
     double value;
@@ -27,30 +13,14 @@ struct vector {
 double** compute_similarity_matrix(struct vector* vectors);
 double* compute_degree_matrix(double** A);
 double** compute_laplacian_matrix(double** A, double* D);
-double** initialize_H(double** W);
-double** optimize_H(double** W);
+double** optimize_H(double** W, double** H, int n, int k);
 
 /* Argument reading and processing functions */
 struct vector* read_data_points(char* file_name);
-void mem_error();
-void free_entries(struct entry *head);
-void free_matrix(double** matrix, int rows);
+
 void print_matrix(double** matrix, int rows, int cols);
 void print_diagonal_matrix(double* diagonal, int size);
+void print_vectors(struct vector *vectors);
 
-/* Computational functions */
-double squared_dist(struct vector u, struct vector v);
-double** matrix_multiply(double** A, double** B, int m, int n, int p);
-double** transpose(double** matrix, int rows, int cols);
-
-
-/* Functions implemented in symnmfmodule.c */
-static PyObject* sym_module_imp(PyObject *self, PyObject *args);
-
-static PyObject* ddg_module_imp(PyObject *self, PyObject *args);
-
-static PyObject* norm_module_imp(PyObject *self, PyObject *args);
-
-static PyObject* symnmf_module_imp(PyObject *self, PyObject *args);
-
-PyMODINIT_FUNC PyInit_symnmf(void);
+int getN();
+int getK();
