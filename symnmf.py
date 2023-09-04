c2eb2ab687bb28cee21cf75606fac04b8c4649c7
romtaugranot <100371590+romtaugranot@users.noreply.github.com>
2023-09-04 15:45:39 +0300

    Update symnmf.py

--- symnmf.py
+++ symnmf.py
@@ -1,7 +1,16 @@
 import sys
 import numpy as np
+import math
 import csv
 import symnmfmodule
+
+
+def print_matrix(matrix):
+    str1 = ''
+
+    for row in matrix:
+        str1 = ','.join(map(lambda x: "%.4f" % x, row))
+        print(str1)
 
 
 # note in 2.9.2 in project_NMF.pdf we are told we don't need to validate args.
@@ -11,14 +20,25 @@ def algorithm():
     goal = sys.argv[2]
     file_path = sys.argv[3]
 
+    result_mat = None
     if goal == 'sym':
-        A = symnmfmodule.sym(file_path)
+        result_mat = symnmfmodule.sym(file_path)
     elif goal == 'ddg':
-        D = symnmfmodule.ddg(file_path)
+        result_mat = symnmfmodule.ddg(file_path)
+        n = len(result_mat)
+        result_mat = [[0 if i != j else result_mat[i] for j in range(n)] for i in range(n)]
     elif goal == 'norm':
-        W = symnmfmodule.norm(file_path)
+        result_mat = symnmfmodule.norm(file_path)
     else: # goal == 'symmnf'
-        H = symnmfmodule.symnmf(file_path)
-
+        W = symnmfmodule.norm(file_path)
+        n = len(W)
+        m = sum(map(sum, W)) / sum(map(len, W)) # average of entries of W.
+        r = 2 * math.sqrt(m) / math.sqrt(k)
+        H = np.random.uniform(low=0, high=r, size=(n,k))
+        H = H.tolist()
+        result_mat = symnmfmodule.symnmf(W, H, n, k)
+    print_matrix(result_mat)
 
 algorithm()
+
+
