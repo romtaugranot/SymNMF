1653131a9f1b4eda81622141667990f0e678251f
romtaugranot <100371590+romtaugranot@users.noreply.github.com>
2023-09-13 21:44:58 +0300

    Finished the c-python part.

--- symnmf.h
+++ symnmf.h
@@ -10,10 +10,16 @@ struct vector {
 };
 
 /* Goals */
-double** compute_similarity_matrix(struct vector* vectors);
+double** compute_similarity_matrix(struct vector* vectors, int n, int d);
 double* compute_degree_matrix(double** A);
 double** compute_laplacian_matrix(double** A, double* D);
 double** optimize_H(double** W, double** H, int n, int k);
+
+/** behind the scenes code**/
+void mem_error();
+void free_entries(struct entry *head);
+void free_vectors(struct vector *head);
+void free_matrix(double** matrix, int rows);
 
 /* Argument reading and processing functions */
 struct vector* read_data_points(char* file_name);
@@ -22,5 +28,4 @@ void print_matrix(double** matrix, int rows, int cols);
 void print_diagonal_matrix(double* diagonal, int size);
 void print_vectors(struct vector *vectors);
 
-int getN();
 int getK();
